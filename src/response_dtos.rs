bf31bae5028545e9c366de3db4f0cd3c11f2f0c7
Ken Greim <34892440+kengreim@users.noreply.github.com>
2024-06-29 16:25:49 -0700

    Update chart group schema to match AviationAPI

--- src/response_dtos.rs
+++ src/response_dtos.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
 pub enum ChartGroup {
     General,
     Departures,
@@ -26,4 +26,62 @@ pub struct ChartDto {
     pub pdf_path: String,
     #[serde(skip_serializing)]
     pub chart_group: ChartGroup,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct GroupedChartsDto {
+    #[serde(rename = "General", skip_serializing_if = "Option::is_none")]
+    pub general: Option<Vec<ChartDto>>,
+    #[serde(rename = "DP", skip_serializing_if = "Option::is_none")]
+    pub departures: Option<Vec<ChartDto>>,
+    #[serde(rename = "STAR", skip_serializing_if = "Option::is_none")]
+    pub arrivals: Option<Vec<ChartDto>>,
+    #[serde(rename = "CAPP", skip_serializing_if = "Option::is_none")]
+    pub approaches: Option<Vec<ChartDto>>,
+}
+
+impl GroupedChartsDto {
+    pub const fn new() -> Self {
+        Self {
+            general: None,
+            departures: None,
+            arrivals: None,
+            approaches: None,
+        }
+    }
+
+    pub fn add_general(&mut self, chart_dto: ChartDto) {
+        match self.general {
+            Some(ref mut charts) => charts.push(chart_dto),
+            None => self.general = Some(vec![chart_dto]),
+        }
+    }
+
+    pub fn add_departure(&mut self, chart_dto: ChartDto) {
+        match self.departures {
+            Some(ref mut charts) => charts.push(chart_dto),
+            None => self.departures = Some(vec![chart_dto]),
+        }
+    }
+
+    pub fn add_arrival(&mut self, chart_dto: ChartDto) {
+        match self.arrivals {
+            Some(ref mut charts) => charts.push(chart_dto),
+            None => self.arrivals = Some(vec![chart_dto]),
+        }
+    }
+
+    pub fn add_approach(&mut self, chart_dto: ChartDto) {
+        match self.approaches {
+            Some(ref mut charts) => charts.push(chart_dto),
+            None => self.approaches = Some(vec![chart_dto]),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum ResponseDto {
+    Charts(Vec<ChartDto>),
+    GroupedCharts(GroupedChartsDto),
 }
