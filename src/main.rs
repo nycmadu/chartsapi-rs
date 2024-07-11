73433475c0702a05b23cd577728ebebc4bd45502
Ken Greim <34892440+kengreim@users.noreply.github.com>
2024-07-11 16:30:16 -0700

    Remove excess space

--- src/main.rs
+++ src/main.rs
@@ -227,7 +227,7 @@ async fn load_charts(current_cycle: &str) -> Result<ChartsHashMaps, anyhow::Erro
     let dtpp = from_str::<DigitalTpp>(&metafile)?;
 
     let eff_start =
-        NaiveDateTime::parse_from_str(&dtpp.from_effective_date, "%H%MZ  %m/%d/%y")?.and_utc();
+        NaiveDateTime::parse_from_str(&dtpp.from_effective_date, "%H%MZ %m/%d/%y")?.and_utc();
     let now = Utc::now();
     debug!("Effective start for charts: {}", eff_start);
     if eff_start > now {
